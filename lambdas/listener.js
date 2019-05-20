857e8c212073abbc0bcec7d8c4c6ea9bdd1f9d21
kmunz@hartenergy.com <kmunz@hartenergy.com>
2019-05-20 09:29:45 -0500

    test for initial SubscribeURL

--- lambdas/listener.js
+++ lambdas/listener.js
@@ -2,15 +2,27 @@
 
 module.exports.handler = async (event) => {
   console.log('Received event:', JSON.stringify(event, null, 2)); // DEBUG
+
   var eventBody = JSON.parse(event.body);
   console.log("eventBody: ",JSON.stringify(eventBody, null, 2));  // DEBUG:
-  var message = JSON.parse(eventBody.Message);
-  console.log("message: ",JSON.stringify(message, null, 2));  // DEBUG:
 
+  // Check if this is the initial subscription event and Log out the SubscribeURL
+  // This URL must be visited before subsequent notifications will be sent to this APIG
+  if(event.multiValueHeaders.hasOwnProperty('x-amz-sns-message-type') && event.multiValueHeaders.x-amz-sns-message-type == "SubscriptionConfirmation") {
+    console.log(`eventBody.Message: ${eventBody.Message}`);
+    console.log(`SubscribeURL: ${eventBody.SubscribeURL}`);
+  } else {
+    // The message is stringified before passing to SNS, which is stringified again by APIG
+    var message = JSON.parse(eventBody.Message);
+    console.log("Message: ",JSON.stringify(message, null, 2));  // DEBUG:
+
+  }
+
+  // Respond to APIG
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: 'Message received!',
       input: event,
     }, null, 2),
   };
